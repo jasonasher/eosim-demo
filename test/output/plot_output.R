a710772a9fed1474f57102468f71b930c19fcb25
EKR <ekr@rtfm.com>
2024-06-11 15:50:13 -0700

    Make plots graph each scenario in a different color.

--- test/output/plot_output.R
+++ test/output/plot_output.R
@@ -4,7 +4,8 @@ incidence_report <- read_csv("test/output/incidence_report.csv")
 
 summarized_incidence_report <- incidence_report %>%
     group_by(scenario, day = floor(time)) %>%
-    summarize(incidence = n())
+    summarize(incidence = n()) %>%
+    mutate(scenario = factor(scenario))
 
 ggplot(summarized_incidence_report) +
-    geom_line(aes(day, incidence, group = scenario))
+    geom_line(aes(x=day, y=incidence, color=scenario))
