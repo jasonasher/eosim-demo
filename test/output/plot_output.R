ed29c1dbbfa092471ead142ea2f56bdd93b00fb4
Ranya Sharma <xsq2@cdc.gov>
2024-06-17 11:51:45 -0500

    merging plot scripts

--- test/output/plot_output.R
+++ test/output/plot_output.R
@@ -1,11 +1,26 @@
 library(tidyverse)
 
 incidence_report <- read_csv("test/output/incidence_report.csv")
+death_report <- read_csv("test/output/death_report.csv")
 
 summarized_incidence_report <- incidence_report %>%
     group_by(scenario, day = floor(time)) %>%
     summarize(incidence = n()) %>%
     mutate(scenario = factor(scenario))
 
-ggplot(summarized_incidence_report) +
-    geom_line(aes(x = day, y = incidence, color = scenario))
+summarized_death_report <- death_report %>%
+  group_by(scenario, day = floor(time)) %>%
+  summarize(death = n()) %>%
+  mutate(scenario = factor(scenario))
+
+death_plot <- ggplot(summarized_death_report) +
+  geom_line(aes(x = day, y = death, color = scenario)) +
+  ggtitle("Death Report")
+
+ggsave("test/output/death_report_plot.png", plot = death_plot)
+
+incidence_plot <- ggplot(summarized_incidence_report) +
+  geom_line(aes(x = day, y = incidence, color = scenario)) +
+  ggtitle("Incidence Report")
+
+ggsave("test/output/incidence_report_plot.png", plot = incidence_plot)
